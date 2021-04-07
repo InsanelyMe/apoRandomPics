3e73ac4e72858bc75444f7c1b04f8dccb5dd7eec
Angel <5618619+InsanelyMe@users.noreply.github.com>
2021-04-07 18:50:17 -0500

    1.01
    Added changing cursors for clarity in UI.
    Randomized random images on every click.
    Added author info on client's request.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,24 @@
-
-// console.log("works?");
-
-
 var PhotoGrid = document.getElementById("photoGrid");
 var BigPhoto = document.getElementById("photoMain");
 var RandomPhoto = document.getElementById("photoRandom");
 
-
 var realityCollapseClickCounter = 0;
 
 function collapseReality(){
-  // console.log("collapsing reality...");
   document.body.innerHTML = "";
   document.body.className += "collapsed";
 }
 
 function showRandom(){
-  // console.log("attempting to show random img");
   let randImg = document.createElement("img");
-  let randURL = "https://picsum.photos/"+RandomPhoto.clientWidth+"/"+RandomPhoto.clientHeight+"/?random&t="+this.tabIndex;
+  let randURL = "https://picsum.photos/"+RandomPhoto.clientWidth+"/"+RandomPhoto.clientHeight+"/?random&t="+Date.now();
   randImg.className = "randIMG";
   randImg.src = randURL;
   RandomPhoto.innerHTML = '';
   RandomPhoto.appendChild(randImg);
+  realityCollapseClickCounter++;
   randImg.onclick = () => {
-    realityCollapseClickCounter++;
-    // console.log("counting towards collapse: "+realityCollapseClickCounter);
+    realityCollapseClickCounter += 3;
     if (realityCollapseClickCounter > 8) {
       collapseReality();
     }
@@ -33,7 +26,6 @@ function showRandom(){
 }
 
 function showBig(){
-  // console.log("clicked on image "+this.tabIndex);
   let bigImg = document.createElement("img");
   bigImg.className = "bigIMG";
   bigImg.src = "imgs/"+this.tabIndex+".jpg";
@@ -43,14 +35,11 @@ function showBig(){
   bigImg.onclick = showRandom;
 }
 
-
 for (var i = 1; i < 7; i++) {
   let newIMG = document.createElement("img");
   newIMG.className = "dummyIMG";
-  // console.log("checking for i: "+i+"?");
   newIMG.src = "imgs/"+i+".jpg";
   newIMG.tabIndex = i;
   PhotoGrid.appendChild(newIMG);
-  // photos.push(newIMG);
   newIMG.onclick = showBig;
 }
